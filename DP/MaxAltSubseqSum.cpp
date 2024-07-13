a20fcbfe1f8b4920fb334856bfc03661831df342
adrijacs <duttaroyadrija123@gmail.com>
2024-07-14 00:11:06 +0530

    Maximum sum of alternate subsequence

--- DP/MaxAltSubseqSum.cpp
+++ DP/MaxAltSubseqSum.cpp
@@ -8,7 +8,7 @@ static int maxSumAlt(int index, vector<int>&arr,vector<int>&dp ){
   if(dp[index]!=-1)return dp[index];
   int pick=arr[index]+maxSumAlt(index-2,arr,dp);
   int nonpick=0 + maxSumAlt(index-1,arr,dp);
-  return max(pick,nonpick);
+  return dp[index]=max(pick,nonpick);
 }
 
 };
@@ -28,3 +28,21 @@ cout<<"Tha maximum sum of alternate subsequence is: "<<Solution::maxSumAlt(n-1,a
 
 
 }
+//Tabulation and space optimisation T(C)=O(n) S(C)=O(1)
+// int maximumNonAdjacentSum(vector<int> &nums){
+//     // Write your code here.
+//     // int n=nums.size();
+//     // vector<int>dp(n,-1);
+//     // return maxSum(n-1,nums,dp);
+//     int prev=nums[0];
+//     int prev2=0;
+//     for(int i=1;i<nums.size();i++){
+//         int pick=nums[i];
+//         if(i>1)pick+=prev2;
+//         int nonpick=0 + prev;
+//         int curr=max(pick,nonpick);
+//          prev2=prev;
+//          prev=curr;
+//     }
+//     return prev;
+// }
