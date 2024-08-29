31f7cc54bb9f4e335293fe40b4b7a8af42a7b079
adrijacs <duttaroyadrija123@gmail.com>
2024-08-29 22:15:27 +0530

    Singly linked list insertions and deletions

--- LinkedList/ll.cpp
+++ LinkedList/ll.cpp
@@ -15,17 +15,191 @@ class Node{
    }
 };
 
-int main(){
-  Node* temp1=new Node(3);
-  Node* temp2=new Node(4);
-  Node* temp3=new Node(5);
-  temp1->next=temp2;
-  temp2->next=temp3;
-  Node* temp=temp1;
-  while(temp!=NULL){
-    cout<<temp->data<<"->";
-    temp=temp->next;
+class LL{
+  private:
+  Node* head;
+  public:
+  LL(){
+    head=nullptr;
   }
+  void insertAtBegin(int data){
+    Node* newNode=new Node(data);
+    if(head==nullptr){
+      head=newNode;
+      return;
+    }
+    newNode->next=head;
+    head=newNode;
+
+  }
+  void insertPos(int val,int k){
+    if(head==NULL){
+      if(k==1){
+        Node* newNode=new Node(val);
+        head=newNode;
+        return;
+      }
+      else{
+        return;
+      }
+    }
+    if(k==1){
+      insertAtBegin(val);
+    }
+    int cnt=0;
+    Node* temp=head;
+    while(temp!=NULL){
+      cnt++;
+      if(cnt==(k-1)){
+        Node* newNode=new Node(val);
+        newNode->next=temp->next;
+        temp->next=newNode;
+        break;
+      }
+      temp=temp->next;
+    }
+  }
+  void insertBeforeValue(int val,int ele){
+    if(head==NULL){
+      cout<<"Linked List is empty";
+      return;
+    }
+     if(head->data==ele){
+      Node* newNode=new Node(val);
+      insertAtBegin(val);
+      return;
+    }
+
+    Node* temp=head;
+    while(temp->next!=NULL){
+     
+      if(temp->next->data==ele){
+        Node* newNode=new Node(val);
+        newNode->next=temp->next;
+        temp->next=newNode;
+        break;
+      }
+      temp=temp->next;
+    }
+  }
+  
+  void insert(int data){
+    Node* newNode=new Node(data);
+     if(head==nullptr){
+      head=newNode;
+      return;
+     }
+     Node* temp=head;
+     while(temp->next!=NULL){
+      temp=temp->next;
+     }
+     temp->next=newNode;
+  }
+  void deleteStart(){
+    if(head==nullptr){
+      return;
+    }
+    Node* temp=head;
+    head=head->next;
+    delete temp;
+  }
+  void deleteEnd(){
+    if(head==nullptr ||head->next==nullptr){
+      delete head;
+      head==nullptr;
+      return;
+    }
+    Node* temp=head;
+    while(temp->next->next!=nullptr){
+      temp=temp->next;
+    }
+    Node* toDelete=temp->next;
+    temp->next=nullptr;
+    delete toDelete;
+  }
+  void deleteK(int k){
+    if(head == NULL)cout<<"Linked List is empty"<<endl;
+    if(k==1){
+      deleteStart();
+      return;
+    }
+    int cnt=0;
+    Node* temp=head;
+    Node* prev=nullptr;
+    while(temp!=NULL){
+      cnt++;
+      if(cnt==k){
+        prev->next=prev->next->next;
+        delete temp;
+        break;
+      }
+      prev=temp;
+      temp=temp->next;
+    }
+  }
+  void deleteEle(int val){
+     if(head == NULL)cout<<"Linked List is empty"<<endl;
+    if(head->data==val){
+      deleteStart();
+      return;
+    }
+  
+    Node* temp=head;
+    Node* prev=nullptr;
+    while(temp!=NULL){
+    
+      if(temp->data==val){
+        prev->next=prev->next->next;
+        delete temp;
+        break;
+      }
+      prev=temp;
+      temp=temp->next;
+    }
+
+  }
+  void display(){
+    if(head==nullptr){
+      cout<<"Linked List is empty";
+      return;
+    }
+    Node* temp=head;
+    while(temp!=NULL){
+      if(temp->next==NULL){
+        cout<<temp->data;
+      }
+      else{
+        cout<<temp->data<<"->";
+      }
+      temp=temp->next;
+    }
+    cout<<endl;
+  }
+
+};
+
+int main(){
+  LL ll;
+  ll.insert(4);
+  ll.insert(5);
+  ll.insert(6);
+  ll.insert(7);
+  ll.insert(8);
+  ll.display();
+  // ll.deleteEnd();
+  // ll.display();
+  // ll.deleteStart();
+  // ll.display();
+  // ll.deleteK(6);
+  // ll.display();
+  ll.deleteEle(4);
+  ll.display();
+  ll.insertAtBegin(2);
+  ll.display();
+  // ll.insertPos(99,4);
+  ll.insertBeforeValue(33,2);
+  ll.display();
+
 
 
 }
